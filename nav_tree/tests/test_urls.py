1647f2021733f93b0a977c84d1cb718d50bdeb42
Charlie Denton <charlie@meshy.co.uk>
2014-08-05 17:55:31 +0100

    Add the base urls file for nav_tree

---
+++ nav_tree/tests/test_urls.py
@@ -0,0 +1,37 @@
+from django.core.urlresolvers import resolve, Resolver404
+from django.test import TestCase
+
+from .. import views
+
+
+class TestNavTreeURLRouter(TestCase):
+    urlconf = 'nav_tree.urls'
+
+    def assert_url_uses_router(self, url):
+        resolved_view = resolve(url, urlconf=self.urlconf)
+        expected_view = views.node_router
+        self.assertEqual(resolved_view.func, expected_view)
+
+    def test_blank_url(self):
+        """Blank urls should not resolve.
+
+        This is actually a test of django, as urls must start with `/`.
+        """
+        with self.assertRaises(Resolver404):
+            self.assert_url_uses_router('')
+
+    def test_root_url(self):
+        self.assert_url_uses_router('/')
+
+    def test_child_url(self):
+        self.assert_url_uses_router('/slug/')
+
+    def test_nested_child_url(self):
+        self.assert_url_uses_router('/foo/bar/')
+
+    def test_numerical_url(self):
+        self.assert_url_uses_router('/meanings/42/')
+
+    def test_without_trailing_slash(self):
+        with self.assertRaises(Resolver404):
+            self.assert_url_uses_router('/fail')
---
+++ nav_tree/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+
+urlpatterns = [
+    # Capture urls that are at the root, or end in a slash.
+    url(r'(?P<url>(^|^.*/))$', 'nav_tree.views.node_router'),
+]
