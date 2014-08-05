b427a679475396abff67781b9c116b2b6f2c7832
Charlie Denton <charlie@meshy.co.uk>
2014-08-05 17:54:19 +0100

    Add nav_tree.views.node_router

---
+++ nav_tree/tests/test_views.py
@@ -0,0 +1,57 @@
+from unittest import mock
+
+from django.http import HttpResponse
+from django.test import TestCase
+
+from .. import views
+from . import factories
+
+
+class RouterTest(TestCase):
+    """Test that `node_router` correctly deals with the urls handed to it."""
+    def test_root(self):
+        url = ''
+        factories.NodeFactory.create()
+        request = mock.MagicMock()
+        handle_path = 'nav_tree.models.Node.handle'
+        with mock.patch(handle_path) as handle:
+            response = views.node_router(request, url)
+
+        handle.assert_called_with(request, '/' + url)
+        self.assertEqual(response, handle(request, '/' + url))
+
+    def test_complex_url(self):
+        url = 'slug/42/foo/bar/'
+        factories.NodeFactory.create()
+        request = mock.MagicMock()
+        handle_path = 'nav_tree.models.Node.handle'
+        with mock.patch(handle_path) as handle:
+            response = views.node_router(request, url)
+
+        handle.assert_called_with(request, '/' + url)
+        self.assertEqual(response, handle(request, '/' + url))
+
+
+class RouterIntegrationTest(TestCase):
+    """Test that `node_router` is correctly handed urls."""
+    def test_root_url(self):
+        url = '/'
+        factories.NodeFactory.create()
+        handle_path = 'nav_tree.models.Node.handle'
+        with self.settings(ROOT_URLCONF='nav_tree.urls'):
+            with mock.patch(handle_path) as handle:
+                handle.return_value = HttpResponse()
+                response = self.client.get(url)
+
+        handle.assert_called_with(response.wsgi_request, url)
+
+    def test_complex_url(self):
+        url = '/slug/42/foo/bar/'
+        factories.NodeFactory.create()
+        handle_path = 'nav_tree.models.Node.handle'
+        with self.settings(ROOT_URLCONF='nav_tree.urls'):
+            with mock.patch(handle_path) as handle:
+                handle.return_value = HttpResponse()
+                response = self.client.get(url)
+
+        handle.assert_called_with(response.wsgi_request, url)
---
+++ nav_tree/views.py
@@ -0,0 +1,10 @@
+from .models import Node
+
+
+def node_router(request, url):
+    """Catch all view that delegates view handling to the best Node match."""
+    # Django strips the leading / when resolving urls, so we'll just go ahead
+    # and add it again. This allows us to use it for resolving later.
+    url = '/' + url
+    node = Node.objects.best_match_for_path(url)
+    return node.handle(request, url)
