c4e720357008c2e2338f7d2659e820958202659e
Charlie Denton <charlie@meshy.co.uk>
2014-09-15 00:29:06 +0100

    Add handler for Pages

--- conman/pages/tests/factories.py
+++ conman/pages/tests/factories.py
@@ -1,11 +1,11 @@
 import factory
 
-from .. import models
+from .. import handlers, models
 from conman.nav_tree.tests.factories import RootNodeFactory
 
 
 class PageFactory(factory.DjangoModelFactory):
-    node = factory.SubFactory(RootNodeFactory)
+    node = factory.SubFactory(RootNodeFactory, handler=handlers.PageHandler.path())
 
     class Meta:
         model = models.Page
