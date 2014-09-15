9e7cbe51483e8858d7a7aa52dcb27361dbc404d6
Charlie Denton <charlie@meshy.co.uk>
2014-09-15 23:34:50 +0100

    Add template for page detail

--- conman/pages/tests/test_views.py
+++ conman/pages/tests/test_views.py
@@ -1,6 +1,6 @@
 from .. import views
 from . import factories
-from conman.tests.utils import RequestTestCase
+from conman.tests.utils import IntegrationTestCase, RequestTestCase
 
 
 class TestPageDetail(RequestTestCase):
@@ -22,3 +22,14 @@ class TestPageDetail(RequestTestCase):
             'view': view,
         }
         self.assertEqual(context, expected)
+
+
+class TestPageDetailIntegration(IntegrationTestCase):
+    view = views.PageDetail
+
+    def test_get(self):
+        page = factories.PageFactory.create(content='This is a test')
+        node = page.node
+        handler = node.get_handler_class()(page.node)
+        response = self.access_view_and_render_response(handler=handler)
+        self.assert_count(page.content, response, 1)
