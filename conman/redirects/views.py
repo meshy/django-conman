2b54c28ef7c7faacbc6ccf85671d0c29360ae930
Charlie Denton <charlie@meshy.co.uk>
2014-09-19 23:11:32 +0100

    Remove unused, misleading class attr

--- conman/redirects/views.py
+++ conman/redirects/views.py
@@ -5,8 +5,6 @@ from .models import NodeRedirect
 
 class NodeRedirectView(RedirectView):
     """Redirect to the target Node."""
-    permanent = False
-
     def get_redirect_url(self, *args, **kwargs):
         redirect = NodeRedirect.objects.get(node=kwargs['handler'].node)
         self.permanent = redirect.permanent
