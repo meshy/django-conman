48c20047c222f7a948dd65f151a174226a401983
Ian Foote <python@ian.feete.org>
2014-10-07 23:39:13 +0100

    Refactor redirects.tests.test_views
    * Split tests for permanent/temporary redirects from test for the
      redirect's target url
    * Simplify integration test to focus on integration
    * Remove essentially duplicate integration test
    * Add docstrings

--- conman/redirects/tests/test_views.py
+++ conman/redirects/tests/test_views.py
@@ -12,25 +12,26 @@ class TestNodeRedirectView(RequestTestCase):
         self.request = self.create_request()
         self.view = views.NodeRedirectView.as_view()
 
+    def test_target(self):
+        """NodeRedirectView redirects to the target's url."""
+        node = ChildNodeRedirectFactory.create(target=self.target)
+        response = self.view(self.request, node=node)
+
+        self.assertEqual(response['Location'], self.target.url)
+
     def test_permanent(self):
-        node = ChildNodeRedirectFactory.create(
-            target=self.target,
-            permanent=True,
-        )
+        """A permanent redirect has status_code 301."""
+        node = ChildNodeRedirectFactory.create(permanent=True)
         response = self.view(self.request, node=node)
 
         self.assertEqual(response.status_code, 301)
-        self.assertEqual(response['Location'], self.target.url)
 
     def test_temporary(self):
-        node = ChildNodeRedirectFactory.create(
-            target=self.target,
-            permanent=False,
-        )
+        """A temporary redirect has status_code 302."""
+        node = ChildNodeRedirectFactory.create(permanent=False)
         response = self.view(self.request, node=node)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['Location'], self.target.url)
 
 
 class TestNodeRedirectViewIntegration(TestCase):
@@ -38,22 +39,9 @@ class TestNodeRedirectViewIntegration(TestCase):
         self.target = ChildNodeFactory.create()
         self.expected = 'http://testserver' + self.target.url
 
-    def test_permanent(self):
-        node = ChildNodeRedirectFactory.create(
-            target=self.target,
-            permanent=True,
-        )
+    def test_access_redirect(self):
+        """Accessing a NodeRedirect's url redirects to its target's url."""
+        node = ChildNodeRedirectFactory.create(target=self.target)
         response = self.client.get(node.url)
 
-        self.assertEqual(response.status_code, 301)
-        self.assertEqual(response['Location'], self.expected)
-
-    def test_temporary(self):
-        node = ChildNodeRedirectFactory.create(
-            target=self.target,
-            permanent=False,
-        )
-        response = self.client.get(node.url)
-
-        self.assertEqual(response.status_code, 302)
         self.assertEqual(response['Location'], self.expected)
