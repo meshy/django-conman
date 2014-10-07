ba0b4ebeeabce88a40d9f86fe0384fb5471f6046
Ian Foote <python@ian.feete.org>
2014-10-07 22:28:40 +0100

    Add docstring to nav_tree.tests.test_urls

--- conman/nav_tree/tests/test_urls.py
+++ conman/nav_tree/tests/test_urls.py
@@ -24,17 +24,22 @@ class TestNavTreeURLRouter(TestCase):
             self.assert_url_uses_router('//')
 
     def test_root_url(self):
+        """The root url is resolved using views.node_router."""
         self.assert_url_uses_router('/')
 
     def test_child_url(self):
+        """A child url is resolved using views.node_router."""
         self.assert_url_uses_router('/slug/')
 
     def test_nested_child_url(self):
+        """A nested child url is resolved using views.node_router."""
         self.assert_url_uses_router('/foo/bar/')
 
     def test_numerical_url(self):
+        """A numeric url is resolved using views.node_router."""
         self.assert_url_uses_router('/meanings/42/')
 
     def test_without_trailing_slash(self):
+        """A url without a trailing slash is not resolved by views.node_router."""
         with self.assertRaises(Resolver404):
             self.assert_url_uses_router('/fail')
