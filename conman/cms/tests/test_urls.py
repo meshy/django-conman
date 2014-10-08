8445d491030be7fb2fa1175140a4b022b2690425
Charlie Denton <charlie@meshy.co.uk>
2014-10-09 00:03:31 +0100

    Add further tests of the cms urls

--- conman/cms/tests/test_urls.py
+++ conman/cms/tests/test_urls.py
@@ -1,6 +1,9 @@
+from unittest import mock
+
+from django.test import TestCase
 from incuna_test_utils.testcases.urls import URLTestCase
 
-from .. import views
+from .. import urls, views
 
 
 class TestCMSIndexURL(URLTestCase):
@@ -11,3 +14,23 @@ class TestCMSIndexURL(URLTestCase):
             '/cms/',
             'cms:index',
         )
+
+
+class TestCMSURLs(TestCase):
+    @mock.patch('conman.cms.urls.url')
+    @mock.patch('conman.cms.urls.include')
+    @mock.patch('django.apps.apps.get_app_config')
+    def test_urls(self, get_app_config, include, url):
+        fake_config = mock.Mock()
+        fake_config.cms_urls = 'example.path.to.urls'
+        fake_config.label = 'example'
+
+        fake_config.managed_apps = {fake_config}
+        get_app_config.return_value = fake_config
+
+        cms_urls = list(urls.urls())
+        expected = [
+            url(r'^$', views.CMSIndex.as_view, name='index'),
+            url(r'^example', include(fake_config.cms_urls))
+        ]
+        self.assertSequenceEqual(cms_urls, expected)
